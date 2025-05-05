c724b99cc4c91d95c22de91ef206ef6dcf04e3a6
lixz00 <1070724184@qq.com>
2025-05-05 16:56:38 -0400

    Update v53_5bench.py

--- GPTQ_llama3.2_eval/v53_5bench.py
+++ GPTQ_llama3.2_eval/v53_5bench.py
@@ -1,7 +1,7 @@
 from gptqmodel import GPTQModel
 from gptqmodel.utils.eval import EVAL
 
-model_id = "Llama-3.2-1B-Instruct-gptqmodel-v53-4bit"
+model_id = "Llama-3.2-1B-Instruct-gptq-v53-4bit"
 
 TASKS = [
     "arc_challenge",   # ARC
