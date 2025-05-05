7c2c004fa4ca63e3055fe0856be321a79d6ec93b
lixz00 <1070724184@qq.com>
2025-05-05 16:55:30 -0400

    Update v53_5bench.py

--- GPTQ_llama3.2_eval/v53_5bench.py
+++ GPTQ_llama3.2_eval/v53_5bench.py
@@ -1,7 +1,7 @@
 from gptqmodel import GPTQModel
 from gptqmodel.utils.eval import EVAL
 
-model_id = "Llama-3.2-1B-Instruct-gptqmodel-4bit"
+model_id = "Llama-3.2-1B-Instruct-gptqmodel-v53-4bit"
 
 TASKS = [
     "arc_challenge",   # ARC
@@ -15,6 +15,6 @@ lm_eval_results = GPTQModel.eval(
     model_id,
     framework=EVAL.LM_EVAL,
     tasks=TASKS,
-    output_file="llama32_1b_4bit_lm_eval.json",
+    output_file="llama32_1b_4bit_v53_5bench.json",
 )
 print(lm_eval_results)
